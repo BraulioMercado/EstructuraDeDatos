6bd45848a4b37999fa1cab6b403425d0787f9f94
BraulioMercado <43398760+BraulioMercado@users.noreply.github.com>
2018-09-24 23:54:52 -0700

    Add files via upload

---
+++ ED-Evaluacion7/ED-Evaluacion7/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED_Evaluacion7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ban = true;
+            do
+            {
+                try
+                {
+                    int contador = 0;
+                    int Nnum = 0;
+
+                    Console.Write("¿Cuantos numeros aleatorios desea ordenar? ");
+                    Nnum = Convert.ToInt32(Console.ReadLine());
+                    int[] vector = new int[Nnum];
+
+                    Random aleatorios = new Random();
+                    for (contador = 0; contador < Nnum; contador++)
+                    {
+                        vector[contador] = Convert.ToInt32(aleatorios.Next(1, 100));
+                    }
+                    Console.Write("Los numeros random son: ");
+                    for (contador = 0; contador < Nnum; contador++)
+                    {
+                        Console.Write("\n" + vector[contador]);
+                    }
+                    Console.Write("\nLos numeros ordenados son: ");
+                    Array.Sort(vector);
+                    Array.Reverse(vector);
+                    for (contador = 0; contador < Nnum; contador++)
+                    {
+                        Console.Write("\n" + vector[contador]);
+                    }
+                    ban = false;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Valor no valido" + e.Message);
+                } 
+            } while (ban==true);
+            Console.ReadKey();
+        }
+    }
+}
