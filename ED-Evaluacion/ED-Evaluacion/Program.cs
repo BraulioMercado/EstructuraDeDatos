f7a0ea6e9635bdb5a8552b0dc9b2d03f99f28673
BraulioMercado <43398760+BraulioMercado@users.noreply.github.com>
2018-09-24 23:48:48 -0700

    Add files via upload

---
+++ ED-Evaluacion/ED-Evaluacion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED_Evaluacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ban = true;
+            do
+            {
+                try
+                {
+                    int contador = 0;
+                    int Nnum = 0;
+
+                    Console.Write("¿Cuantos numeros desea ingresar? ");
+                    Nnum = Convert.ToInt32(Console.ReadLine());
+                    int[] valores = new int[Nnum];
+
+                    for (contador = 0; contador < Nnum; contador++)
+                    {
+                        Console.Write("Ingrese valor {0}: ", (contador + 1));
+                        valores[contador] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.Write("El valor Maximo es: " + valores.Max());
+                    Console.Write("\nEl valor Minimo es: " + valores.Min());
+                    ban = false;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Valor no valido" + e.Message);
+                } 
+            } while (ban==true);
+            Console.ReadKey();
+
+        }
+    }
+}
