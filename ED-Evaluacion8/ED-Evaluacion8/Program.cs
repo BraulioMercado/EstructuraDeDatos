200fa59035c788f10a0032f2107e165ae36ab5bf
BraulioMercado <43398760+BraulioMercado@users.noreply.github.com>
2018-09-24 23:55:23 -0700

    Add files via upload

---
+++ ED-Evaluacion8/ED-Evaluacion8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED_Evaluacion8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ban = true;
+            do
+            {
+                int palindromo = 0;
+                Console.Write("Ingrese numero de 5 digitos: ");
+                palindromo = int.Parse(Console.ReadLine());
+                if (palindromo >= 10000 && palindromo <= 99999)
+                {
+                    if(palindromo.ToString()[0]==palindromo.ToString()[4] && palindromo.ToString()[1]==palindromo.ToString()[3])
+                    {
+                        Console.Write("Es palindromo");
+                    }
+                    else
+                    {
+                        Console.Write("No es palindromo");
+                    }
+                    ban = false;
+
+                }
+                else
+                {
+                    Console.Write("No es un numero de 5 digitos.\n");
+                }
+            }
+            while (ban == true);
+
+            Console.ReadKey();
+        }
+    }
+}
